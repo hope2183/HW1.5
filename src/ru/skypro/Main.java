cb7ad1d2c49b6e55b360bdd3894da2a7587fa170
hope2183@mail.ru <hope2183@mail.ru>
2021-12-01 11:45:44 +1100

    Branch Commit 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,10 +3,10 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// TASK 1
+        // TASK 1
 
-        int []numbers = new int [3];
-        numbers [0] = 1;
+        int[] numbers = new int[3];
+        numbers[0] = 1;
         numbers[1] = 2;
         numbers[2] = 3;
 
@@ -14,85 +14,85 @@ public class Main {
 
         float[] myVariable = {0.25f, 0.75f, 1.1f};
 
-    // TASK 2
-        for (int i = 0; i < numbers.length; i++){
-            if (i!= numbers.length-1) {
+        // TASK 2
+        for (int i = 0; i < numbers.length; i++) {
+            if (i != numbers.length - 1) {
                 System.out.print(numbers[i] + ", ");
-            }else
+            } else
                 System.out.print(numbers[i]);
 
         }
         System.out.println();
 
-            for (int i = 0; i < fractions.length; i++) {
-            if (i!= fractions.length-1) {
+        for (int i = 0; i < fractions.length; i++) {
+            if (i != fractions.length - 1) {
                 System.out.print(fractions[i] + ", ");
             } else
                 System.out.print(fractions[i]);
-            }
+        }
         System.out.println();
 
         for (int i = 0; i < myVariable.length; i++) {
-            if (i!= myVariable.length-1){
-                System.out.print(myVariable[i]+", ");
+            if (i != myVariable.length - 1) {
+                System.out.print(myVariable[i] + ", ");
             } else
                 System.out.print(myVariable[i]);
         }
 
         System.out.println();
 
-    // TASK 3
+        // TASK 3
 
-        for (int i = numbers.length-1; i >=0; i--) {
-            if (i!=0) {
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (i != 0) {
                 System.out.print(numbers[i] + ", ");
-            }   else
+            } else
                 System.out.print(numbers[i]);
-            }
+        }
         System.out.println();
 
-        for (int i = fractions.length-1; i >=0 ; i--) {
-            if (i!=0){
-                System.out.print(fractions[i]+", ");
+        for (int i = fractions.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(fractions[i] + ", ");
             } else
                 System.out.print(fractions[i]);
         }
         System.out.println();
 
-        for (int i = myVariable.length-1; i >=0 ; i--) {
-            if (i!=0){
-                System.out.print(myVariable[i]+", ");
+        for (int i = myVariable.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(myVariable[i] + ", ");
             } else
                 System.out.print(myVariable[i]);
         }
 
         System.out.println();
-    //TASK 4
+        //TASK 4
 
         for (int i = 0; i < numbers.length; i++) {
-            if (i!= numbers.length-1&&numbers[i]%2==0) {
+            if (i != numbers.length - 1 && numbers[i] % 2 == 0) {
                 System.out.print(numbers[i] + ", ");
-            }else if (i!= numbers.length-1&&numbers[i]%2!=0){
-                numbers[i]+=1;
+            } else if (i != numbers.length - 1 && numbers[i] % 2 != 0) {
+                numbers[i] += 1;
                 System.out.print(numbers[i] + ", ");
-            }else if (i== numbers.length-1&&numbers[i]%2!=0){
-                numbers[i]+=1;
+            } else if (i == numbers.length - 1 && numbers[i] % 2 != 0) {
+                numbers[i] += 1;
                 System.out.print(numbers[i]);
-            }else
+            } else
                 System.out.print(numbers[i]);
         }
         System.out.println();
-            //ИЛИ
+        //ИЛИ
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i]%2!=0){
-                numbers[i]+=1;
-            }else if (i!= numbers.length-1){
+            if (numbers[i] % 2 != 0) {
+                numbers[i] += 1;
+            } else if (i != numbers.length - 1) {
                 System.out.print(numbers[i] + ", ");
-            }else{
+            } else {
                 System.out.print(numbers[i]);
             }
         }
-            }
+    }
 
     }
 
